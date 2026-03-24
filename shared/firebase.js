a2a8c4618279d24786dac25ba8d3c1475e0c7687
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-03-24 14:39:00 +0100

    Update firebase.js

--- shared/firebase.js
+++ shared/firebase.js
@@ -2,20 +2,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
 
-// Jouw config (die je stuurde)
 const firebaseConfig = {
   apiKey: "AIzaSyDYzfeqkMwg8V0Ok_cXb1arqgEWcKVfbfM",
   authDomain: "jtvradio.firebaseapp.com",
-  databaseURL: "https://jtvradio-default-rtdb.firebaseio.com", // 👈 BELANGRIJK TOEGEVOEGD
+  databaseURL: "https://jtvradio-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "jtvradio",
   storageBucket: "jtvradio.firebasestorage.app",
   messagingSenderId: "471273476911",
   appId: "1:471273476911:web:e0d25926131212e6ec87da"
 };
 
-// Init
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Export zodat andere files het kunnen gebruiken
 export { db, ref, set, onValue };
