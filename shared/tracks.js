7bd760fcd4a13b72bcefe4019ab17b5b7a01bae0
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-03-24 14:25:59 +0100

    Create tracks.js

---
+++ shared/tracks.js
@@ -0,0 +1,14 @@
+export const tracks = {
+  1: {
+    title: "First Song",
+    artist: "Artist 1",
+    file: "../songs/1.mp3",
+    duration: 180
+  },
+  2: {
+    title: "Second Song",
+    artist: "Artist 2",
+    file: "../songs/2.mp3",
+    duration: 200
+  }
+};
